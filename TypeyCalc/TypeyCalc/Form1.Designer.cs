70b61669ba266784ee7906c57fa3c60054173e81
wellthatsummokay <40923193+wellthatsummokay@users.noreply.github.com>
2020-12-28 15:14:38 -0500

    TypeyCalc

---
+++ TypeyCalc/TypeyCalc/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TypeyCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyJustAnswerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyJustPinnedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.replacePinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Light", 26F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 111);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.ContextMenuStrip = this.contextMenuStrip1;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 26F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(494, 96);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.copyJustAnswerToolStripMenuItem,
+            this.copyJustPinnedToolStripMenuItem,
+            this.pinToolStripMenuItem,
+            this.replacePinToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(168, 114);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // copyJustAnswerToolStripMenuItem
+            // 
+            this.copyJustAnswerToolStripMenuItem.Name = "copyJustAnswerToolStripMenuItem";
+            this.copyJustAnswerToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.copyJustAnswerToolStripMenuItem.Text = "Copy Just Answer";
+            this.copyJustAnswerToolStripMenuItem.Visible = false;
+            this.copyJustAnswerToolStripMenuItem.Click += new System.EventHandler(this.copyJustAnswerToolStripMenuItem_Click);
+            // 
+            // copyJustPinnedToolStripMenuItem
+            // 
+            this.copyJustPinnedToolStripMenuItem.Name = "copyJustPinnedToolStripMenuItem";
+            this.copyJustPinnedToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.copyJustPinnedToolStripMenuItem.Text = "Copy Just Pinned";
+            this.copyJustPinnedToolStripMenuItem.Visible = false;
+            this.copyJustPinnedToolStripMenuItem.Click += new System.EventHandler(this.copyJustPinnedToolStripMenuItem_Click);
+            // 
+            // pinToolStripMenuItem
+            // 
+            this.pinToolStripMenuItem.Name = "pinToolStripMenuItem";
+            this.pinToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.pinToolStripMenuItem.Text = "Pin";
+            this.pinToolStripMenuItem.Click += new System.EventHandler(this.pinToolStripMenuItem_Click);
+            // 
+            // replacePinToolStripMenuItem
+            // 
+            this.replacePinToolStripMenuItem.Name = "replacePinToolStripMenuItem";
+            this.replacePinToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.replacePinToolStripMenuItem.Text = "Replace Pin";
+            this.replacePinToolStripMenuItem.Visible = false;
+            this.replacePinToolStripMenuItem.Click += new System.EventHandler(this.replacePinToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(518, 206);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TypeyCalc";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyJustAnswerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyJustPinnedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem replacePinToolStripMenuItem;
+    }
+}
+
---
+++ TypeyCalc/TypeyCalc/Form1.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace TypeyCalc
+{
+    public partial class Form1 : Form
+    {
+        Timer Timer = new Timer();
+        bool PinnedAnswer = false;
+        string PinnedAnswerText;
+        string Ans;
+        public Form1()
+        {
+            InitializeComponent();
+            Timer.Interval = 500;
+            Timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.DarkRed;
+            //label1.Text = "";
+            Timer.Stop();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            object Answer;
+            try
+            {
+                Answer = Evaluate(RemoveComments(textBox1.Text.ToLower().Replace('x', '*').Replace("e+", "*10^").Replace("e", "*10^")));
+            } catch
+            {
+                Answer = null;
+            }
+            if (Answer != null)
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                if (!PinnedAnswer)
+                {
+                    label1.Text = Answer.ToString();
+                } else
+                {
+                    label1.Text = PinnedAnswerText + " // " + Answer.ToString();
+                }
+                Ans = Answer.ToString();
+            } else
+            {
+                Timer.Start();
+            }
+            if (textBox1.Text.ToLower().Replace(" ", "") == "typeycalc")
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                label1.Text = "Amazing";
+                Ans = "Amazing";
+            }
+            if (textBox1.Text.ToLower().Replace(" ", "") == "logan")
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                label1.Text = "Idiot";
+                Ans = "Idiot";
+            }
+            if (textBox1.Text.ToLower().Replace(" ", "") == "typeycalc=amazing")
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                label1.Text = "True";
+                Ans = "True";
+            }
+            if (textBox1.Text.ToLower().Replace(" ", "") == "typeycalc=bad")
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                label1.Text = "False";
+                Ans = "False";
+            }
+            if (textBox1.Text.ToLower().Replace(" ", "") == "logan=idiot")
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                label1.Text = "True";
+                Ans = "True";
+            }
+            if (textBox1.Text.ToLower().Replace(" ", "") == "logan=notidiot")
+            {
+                Timer.Stop();
+                label1.ForeColor = SystemColors.ControlText;
+                label1.Text = "False";
+                Ans = "False";
+            }
+        }
+
+        private string RemoveComments(string Input)
+        {
+            string Output = "";
+            bool InComment = false;
+            foreach (Char c in Input)
+            {
+                if (c == '\\')
+                {
+                    if (!InComment)
+                    {
+                        InComment = true;
+                    } else
+                    {
+                        InComment = false;
+                    }
+                }
+                if (!InComment && c != '\\')
+                {
+                    Output = Output + c;
+                }
+            }
+            return Output;
+        }
+
+        private object Evaluate(string Input)
+        {
+            string withoutSpaces = Input.Replace(" ", "");
+            if (withoutSpaces == "")
+            {
+                return (double)0;
+            }
+            List<object> SeperateTerms = new List<object>();
+            string Temp = "";
+            int ParenthaseLevel = 0;
+            foreach (char c in withoutSpaces)
+            {
+                if (c == '(')
+                {
+                    ParenthaseLevel++;
+                    if (SeperateTerms.Count == 0)
+                    {
+                        SeperateTerms.Add("");
+                    }
+                    if (SeperateTerms.Last().GetType() != typeof(string))
+                    {
+                        SeperateTerms.Add("");
+                    }
+                    if (ParenthaseLevel == 1)
+                    {
+                        continue;
+                    }
+                }
+                if (c == ')')
+                {
+                    ParenthaseLevel--;
+                    if (ParenthaseLevel < 0)
+                    {
+                        throw new FormatException();
+                    }
+                    if (ParenthaseLevel == 0)
+                    {
+                        //SeperateTerms[SeperateTerms.Count - 1] = (string)SeperateTerms.Last() + c;
+                        continue;
+                    }
+                }
+                if (ParenthaseLevel == 0)
+                {
+                    if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '.')
+                    {
+                        Temp = Temp + c;
+                    }
+                    else
+                    {
+                        if (Temp != "")
+                        {
+                            double Out;
+                            if (double.TryParse(Temp, out Out))
+                            {
+                                SeperateTerms.Add(Out);
+                            }
+                            else
+                            {
+                                throw new FormatException();
+                            }
+                            Temp = "";
+                        }
+                        if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '=')
+                        {
+                            SeperateTerms.Add(c);
+                        }
+                        else
+                        {
+                            throw new FormatException();
+                        }
+                    }
+                }
+                else
+                {
+                    SeperateTerms[SeperateTerms.Count - 1] = (string)SeperateTerms.Last() + c;
+                }
+            }
+            if (Temp != "")
+            {
+                double Out;
+                if (double.TryParse(Temp, out Out))
+                {
+                    SeperateTerms.Add(Out);
+                }
+                Temp = "";
+            }
+            //Negatives
+            for (int i = 0; i < SeperateTerms.Count; i++)
+            {
+                if (i == 0 && SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '-')
+                {
+                    if (SeperateTerms.Count - 1 == i || SeperateTerms[i + 1].GetType() != typeof(double))
+                    {
+                        throw new FormatException();
+                    }
+                    SeperateTerms[i] = "0 - " + SeperateTerms[i + 1].ToString();
+                    SeperateTerms.RemoveAt(i + 1);
+                    i = -1;
+                }
+                if (i > 0 && SeperateTerms.Count - 1 != i && SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '-' && (SeperateTerms[i - 1].GetType() != typeof(double) && SeperateTerms[i - 1].GetType() != typeof(string)) && SeperateTerms[i + 1].GetType() == typeof(double))
+                {
+                    SeperateTerms[i] = "0 - " + SeperateTerms[i + 1].ToString();
+                    SeperateTerms.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+            /*foreach (Object o in SeperateTerms)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("------------------------------------");*/
+
+            //Parenthases
+            for (int i = 0; i < SeperateTerms.Count; i++)
+            {
+                if (SeperateTerms[i].GetType() == typeof(string))
+                {
+                    object toReplace = Evaluate((string)SeperateTerms[i]);
+                    SeperateTerms[i] = toReplace;
+                    i = -1;
+                    continue;
+                }
+            }
+            /*foreach (Object o in SeperateTerms)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("------------------------------------");*/
+
+            //Exponents
+            for (int i = 0; i < SeperateTerms.Count; i++)
+            {
+                if (SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '^')
+                {
+                    if (!(i > 0 && i < SeperateTerms.Count - 1 && SeperateTerms[i - 1].GetType() == typeof(double) && SeperateTerms[i + 1].GetType() == typeof(double)))
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        double toReplace = Math.Pow((double)SeperateTerms[i - 1], (double)SeperateTerms[i + 1]);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms[i - 1] = toReplace;
+                        i = -1;
+                        continue;
+                    }
+                }
+            }
+            /*foreach (Object o in SeperateTerms)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("------------------------------------");*/
+
+            //Multiplication & Division
+            for (int i = 0; i < SeperateTerms.Count; i++)
+            {
+                if (SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '*')
+                {
+                    if (!(i > 0 && i < SeperateTerms.Count - 1 && SeperateTerms[i - 1].GetType() == typeof(double) && SeperateTerms[i + 1].GetType() == typeof(double)))
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        double toReplace = (double)SeperateTerms[i - 1] * (double)SeperateTerms[i + 1];
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms[i - 1] = toReplace;
+                        i = -1;
+                        continue;
+                    }
+                }
+                if (SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '/')
+                {
+                    if (!(i > 0 && i < SeperateTerms.Count - 1 && SeperateTerms[i - 1].GetType() == typeof(double) && SeperateTerms[i + 1].GetType() == typeof(double)))
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        double toReplace = (double)SeperateTerms[i - 1] / (double)SeperateTerms[i + 1];
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms[i - 1] = toReplace;
+                        i = -1;
+                        continue;
+                    }
+                }
+            }
+            /*foreach (Object o in SeperateTerms)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("------------------------------------");*/
+
+            //Addition & Subtraction
+            for (int i = 0; i < SeperateTerms.Count; i++)
+            {
+                if (SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '+')
+                {
+                    if (!(i > 0 && i < SeperateTerms.Count - 1 && SeperateTerms[i - 1].GetType() == typeof(double) && SeperateTerms[i + 1].GetType() == typeof(double)))
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        double toReplace = (double)SeperateTerms[i - 1] + (double)SeperateTerms[i + 1];
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms[i - 1] = toReplace;
+                        i = -1;
+                        continue;
+                    }
+                }
+                if (SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '-')
+                {
+                    if (!(i > 0 && i < SeperateTerms.Count - 1 && SeperateTerms[i - 1].GetType() == typeof(double) && SeperateTerms[i + 1].GetType() == typeof(double)))
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        double toReplace = (double)SeperateTerms[i - 1] - (double)SeperateTerms[i + 1];
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms[i - 1] = toReplace;
+                        i = -1;
+                        continue;
+                    }
+                }
+            }
+            /*foreach (Object o in SeperateTerms)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("------------------------------------");*/
+
+            //Equal to
+            for (int i = 0; i < SeperateTerms.Count; i++)
+            {
+                if (SeperateTerms[i].GetType() == typeof(char) && (char)SeperateTerms[i] == '=')
+                {
+                    if (!(i > 0 && i < SeperateTerms.Count - 1))
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        bool toReplace = (SeperateTerms[i - 1].Equals(SeperateTerms[i + 1]));
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms.RemoveAt(i - 1);
+                        SeperateTerms[i - 1] = toReplace;
+                        i = -1;
+                        continue;
+                    }
+                }
+            }
+
+            if (SeperateTerms.Count == 0)
+            {
+                throw new Exception();
+            }
+            if (SeperateTerms[0].GetType() == typeof(char) || SeperateTerms[0].GetType() == typeof(string))
+            {
+                throw new Exception();
+            }
+            if (SeperateTerms.Count != 1)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                return SeperateTerms[0];
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label1.Text);
+        }
+
+        private void pinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!PinnedAnswer)
+            {
+                PinnedAnswerText = Ans;
+                PinnedAnswer = true;
+                pinToolStripMenuItem.Text = "Unpin";
+                copyToolStripMenuItem.Text = "Copy All";
+                copyJustAnswerToolStripMenuItem.Visible = true;
+                copyJustPinnedToolStripMenuItem.Visible = true;
+                replacePinToolStripMenuItem.Visible = true;
+                textBox1_TextChanged(null, null);
+            } else
+            {
+                PinnedAnswer = false;
+                pinToolStripMenuItem.Text = "Pin";
+                copyToolStripMenuItem.Text = "Copy";
+                copyJustAnswerToolStripMenuItem.Visible = false;
+                copyJustPinnedToolStripMenuItem.Visible = false;
+                replacePinToolStripMenuItem.Visible = false;
+                textBox1_TextChanged(null, null);
+            }
+        }
+
+        private void copyJustAnswerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Ans);
+        }
+
+        private void copyJustPinnedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(PinnedAnswerText);
+        }
+
+        private void replacePinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PinnedAnswerText = Ans;
+            textBox1_TextChanged(null, null);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            textBox1.Width = this.Width - 40;
+            label1.Width = this.Width - 40;
+            textBox1.Height = this.Height - 134;
+            label1.Location = new Point(label1.Location.X, textBox1.Height - 10);
+        }
+    }
+}
